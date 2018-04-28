0126ce54330cc4384f9cfea1a751d7da14376333
jhugobb <hugo.b.brandao@gmail.com>
2018-04-28 19:54:27 +0200

    Remove build folder from repo

--- build/CMakeFiles/runa_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "runa_automoc.cpp"
-  "CMakeFiles/runa_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/runa_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
